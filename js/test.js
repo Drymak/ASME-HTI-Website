935c02118a9b101841d379ecc49ab66a17ecfc6e
luayalzieny <62260966+luayalzieny@users.noreply.github.com>
2020-11-08 19:43:32 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,7 @@
 const request=require("request");
 const bodyParser=require("body-parser");
 const express=require("express")
+const mongoose=require('mongoose')
 const app=express()
 const fs=require("fs")
 const path = require('path')
@@ -8,13 +9,38 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(express.static("../../ASME-HTI-Website"))
 
-app.get("/" ,function(req,res){
+
+mongoose.connect("mongodb+srv://AsmeDB:asme2020DB@cluster0.vt4oj.mongodb.net/AsmeDB?retryWrites=true&w=majority",{
+	useNewUrlParser:true,
+useUnifiedTopology:true,
+useFindAndModify:true})
+
+const asmeSchema=new mongoose.Schema({
+	name:{type:String}
+	,HTI_id:{type:String}
+	,mob:{type:String}
+	,university:{type:String}
+	,faculty:{type:String}
+	,What_will_you_learn_from_ASME_HTI:{type:String}
+	,email:{type:String}
+	,first_committee:{type:String}
+	,why_first_committee:{type:String}
+	,second_committee:{type:String}
+	,why_first_committee:{type:String}
+	,experience:{type:String}
+	,comments:{type:String}
+
+})
+
+const student=mongoose.model("student",asmeSchema)
+
+app.get("/registeration_asme" ,function(req,res){
 	res.sendFile('registration.html', {root: '../Pages'})
 
 })
 
-	for(var i=0;i<=1;i++){
-app.post("/",function(req,res){
+//	for(var i=0;i<=1;i++){
+app.post("/registeration_asme",function(req,res){
 	var name=req.body.Name
 	var id=req.body.ID
 	var mob=req.body.Mob
@@ -28,47 +54,28 @@ var second=req.body.Second
 	var why2=req.body.why2
 	var experience=req.body.experience
 	var comments=req.body.comments
-	console.log(name)
-//	console.log(id)
-//	console.log(email)
-//	console.log(mob)
-//	console.log(uni)
-//	console.log(fac)
-//	console.log(first)
-//	console.log(second)
-//	console.log(why)
-//	console.log(why2)
-//	console.log(experience)
-//	console.log(comments)
-	var account={"Name":name
-				 ,"ID":id,
-				 "email":email
-				,"Mobile":mob
-				,"University":uni
-				,"Faculty":fac
-				 ,"What will you learn from ASME-HTI?":learn
-				 ,"First Commitee":first
-				 ,"why the first commitee":why
-				 ,"Second Commitess":second
-				 ,"why the second commitee":why2
-				 ,"experience":experience
-				 ,"comments":comments
+	
 
-				}
+const entry=new student({
+	name:name
+	,HTI_id:id
+	,email:email
+	,mob:mob
+	,university:uni
+	,faculty:fac
+	,What_will_you_learn_from_ASME_HTI:learn
+	,first_committee:first
+	,why_first_committee:why
+	,second_committee:second
+	,why_first_committee:why2
+	,experience:experience
+	,comments:comments
 
-	fs.readFile('Registration.json', function (err, data) {
-    var dat = JSON.parse(data)
-
-	dat.push(account)
-
-
-    fs.writeFile("Registration.json", JSON.stringify(dat,null,4),
-				 function(){console.log("done")
-	})
 })
+entry.save()
 
 	res.sendFile('success.html', {root: '../Pages/'})
-		}	)}
+		}	)
 
 
 app.listen(3000,function(){
